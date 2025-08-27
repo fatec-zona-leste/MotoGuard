6ed8ea360cd7a338973c2bff8b9f9a2aa70cc68f
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-08-27 09:31:35 -0300

    refact: remover api do whatsapp e integrar no backend

--- message-api/src/utils/vars.ts
+++
@@ -1,4 +0,0 @@
-export const PORT = process.env.PORT ?? 8081;
-export const APP_ENV = process.env.APP_ENV ?? "prod";
-export const APP_URL = process.env.APP_URL || `http://localhost:${PORT}`;
-export const ALLOW_ORIGINS = process.env.ALLOW_ORIGINS?.split(",") || "*";
