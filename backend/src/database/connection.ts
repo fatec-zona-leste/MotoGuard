9ad14e1874225f856a5992325b45fcd9685adf7a
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-09-03 19:44:44 -0300

    refact: remover qrcode-generator da branch principal

--- backend/src/database/connection.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-import 'dotenv/config';
-import { DATABASE_HOST, DATABASE_NAME, DATABASE_PASSWORD, DATABASE_USERNAME } from "../utils/vars";
-
-export const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, {
-  host: DATABASE_HOST,
-  dialect: "mysql"
-});
