7c45a18b196b48d8b0a47033639a37be9ea4ed7d
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-09-04 15:54:02 -0300

    feat: qrcode de sensor de proximidade

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import qrcode
 import json
 
-esp_id = input("Digite o ID único ESP32 (ex: a8b2d08c): ")
+esp_id = input("Digite o ID único ESP32 (ex: REAR_SENSOR_a9b2d08c): ")
 
-# data = {
-#     "SERVICE_UUID": "12345678-1234-1234-1234-1234567890ab",
-#     "CHARACTERISTIC_UUID": "abcdef01-2345-6789-abcd-ef0123456789",
-#     "IMPACT_BLUETOOTH_NAME": f"ESP32C3_{esp_id}",
-#     "DISTANCE_BLUETOOTH_NAME": f"ESP32C3_{esp_id}",
-# }
 data = {
+    "BLUETOOTH_NAME": esp_id,
     "SERVICE_UUID": "12345678-1234-1234-1234-1234567890ab",
     "CHARACTERISTIC_UUID": "abcdef01-2345-6789-abcd-ef0123456789",
-    "BLUETOOTH_NAME": f"ESP32C3_a8b2d08c",
 }
 
 json_data = json.dumps(data) # Converte para JSON
 img = qrcode.make(json_data) # Gera QRCode
-img.save("esp32_qrcode.png") # Salva QRCode em imagem
+img.save(f"{esp_id}.png") # Salva QRCode em imagem
 
-print("QRCode gerado com sucesso: esp32_qrcode.png")
+print(f"QRCode gerado com sucesso: {esp_id}.png")
